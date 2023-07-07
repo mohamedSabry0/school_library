2421e1cfafb99ffad5e38a2d366a709a2694a95d
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-07-07 19:07:28 +0300

    Create Student class

--- main.rb
+++ main.rb
@@ -1,11 +1,15 @@
 require './person'
+require './student'
 
 def main
   first = Person.new(14)
-  second = Person.new(18)
+  second = Student.new(18, 'Ruby')
   p first, second
   p first.can_use_services?
   p second.can_use_services?
+  second.age = 21
+  p second
+  p second.play_hooky
 end
 
 main
