f17c6a74d591032f7da8c4dabbb4efa40eb333ba
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-07-07 19:30:57 +0300

    Create Teacher class

--- main.rb
+++ main.rb
@@ -1,15 +1,20 @@
 require './person'
 require './student'
+require './teacher'
 
 def main
   first = Person.new(14)
   second = Student.new(18, 'Ruby')
+  teacher1 = Teacher.new('Jack', 45, 'Math')
   p first, second
   p first.can_use_services?
   p second.can_use_services?
   second.age = 21
   p second
   p second.play_hooky
+  p teacher1
+  p teacher1.can_use_services?
+
 end
 
 main
